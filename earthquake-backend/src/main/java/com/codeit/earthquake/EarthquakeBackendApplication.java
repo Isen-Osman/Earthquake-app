370f9287688526e9d9f9f23a2dd46abb3a4db679
isendev <isen.osman252@gmail.com>
2026-04-15 10:39:55 +0200

    Initial commit

---
+++ earthquake-backend/src/main/java/com/codeit/earthquake/EarthquakeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.earthquake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarthquakeBackendApplication.class, args);
+    }
+
+}
---
+++ earthquake-backend/src/test/java/com/codeit/earthquake/EarthquakeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.earthquake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthquakeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
